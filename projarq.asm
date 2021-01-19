9d46e58e4df967ca35023f0938f36e3845706d03
Daniel Rosa <dan.rosa10@gmail.com>
2021-01-19 16:12:50 -0300

    quase pronto

--- projarq.asm
+++ projarq.asm
@@ -1,6 +1,7 @@
 
 .data
 	localArquivo: .asciiz "C:\\Users\\danro\\Downloads\\arquivo.txt"
+	arqSaida: .asciiz "C:\\Users\\danro\\Downloads\\arquivoSaida.txt"
 	input:	.space	512
 	output:	.space	512
 
@@ -8,74 +9,111 @@
 .globl main
 main:
 
-	li $v0, 13 				# abrir arquivo
-	la $a0, localArquivo 			# caminho do arquivo
-	li $a1, 0 				# arquivo para leitura
-	syscall 				# descritor(REGISTRADOR/buffer QUE GUARDA O ARQUIVO) em $v0
+	li $v0, 13 					# Abrir arquivo
+	la $a0, localArquivo 				# Caminho do arquivo
+	li $a1, 0 					# Arquivo para leitura
+	syscall 					# Descritor(REGISTRADOR/buffer QUE GUARDA O ARQUIVO) em $v0
 	
-	move $s0, $v0				# Colocar o descritor em $a0
+	move $s0, $v0					# Colocar o descritor em $a0
 
-	li $v0, 14 				# ler o conteudo referenciado por $a0
-	move $a0, $s0
-	la $a1, input 				# buffer que armazena o conteudo
-	li $a2, 1024 				# tamanho do buffer = deve ser maior que o tam do arq
-	syscall 				# leitura de TODO o arquivo, colocando a informação no buffer
+	li 	$v0, 14 				# Ler o conteudo referenciado por $a0
+	move 	$a0, $s0
+	la 	$a1, input 				# Buffer que armazena o conteudo
+	li 	$a2, 1024 				# Tamanho do buffer = deve ser maior que o tam do arq
+	syscall 					# Leitura de TODO o arquivo, colocando a informação no buffer
+	la 	$a0, input				# a0 = palavra
+	
+	li 	$t7, 0					# int t7 = tamanho das palavras até ele
+	
+	loop:
+		jal 	find_word
+	
+		add	$t6, $zero, $v0			# t6 = tamanho da palavra atual + \n
+		add	$t7, $t7, $t6
+		add	$t1, $zero, $t7			# t1 = length
+		add	$t2, $zero, $a0			# t2 = palavra
+	
+		jal	reverse_word
+	
+		addi	$t6, $t6, 1			# inicio da prox palavra
+		addi	$t7, $t7, 1
+		add	$a0, $a0, $t6
+		
+		li	$t0, 0
+		li	$t2, 0
+		add	$t2, $a0, $t0			# t2 = inicio_palavra + t0
+		lb	$t1, 0($t2)
+		beqz	$t1, exit			# We found the null-byte
+		j	loop
 
-	move $a0, $a1
-	
-	jal	strlen				# JAL to strlen function, saves return address to $ra
-	
-	add	$t1, $zero, $v0			# Copy some of our parameters for our reverse function
-	add	$t2, $zero, $a0			# We need to save our input string to $t2, it gets
-	add	$a0, $zero, $v0			# butchered by the syscall.
-	li	$v0, 1				# This prints the length that we found in 'strlen'
-	syscall
-	
-reverse:
-	li	$t0, 0				# Set t0 to zero to be sure
-	li	$t3, 0				# and the same for t3
-	
-	reverse_loop:
-		add	$t3, $t2, $t0		# $t2 is the base address for our 'input' array, add loop index
-		lb	$t4, 0($t3)		# load a byte at a time according to counter
-		beqz	$t4, exit		# We found the null-byte
-		sb	$t4, output($t1)	# Overwrite this byte address in memory	
-		subi	$t1, $t1, 1		# Subtract our overall string length by 1 (j--)
-		addi	$t0, $t0, 1		# Advance our counter (i++)
-		j	reverse_loop		# Loop until we reach our condition
-	
-exit:
-	li	$v0, 4				# Print
-	la	$a0, output			# the string!
-	syscall
-	
-	li $v0, 16
-	move $a0, $s0 				#Devolve pra a0 o descritor, necessário pra fechar o arquivo
-	syscall					#fechar o arquivo
+	exit:	
+		li	$v0, 4				# Print
+		la	$a0, output			# the string!
+		syscall
 		
-	li	$v0, 10				# exit()
-	syscall
+    		li $v0, 13 				# abrir arquivo Saida
+    		la $a0, arqSaida 			# caminho do arquivo
+    		li $a1, 1 				# arquivo para escrita
+    		syscall 				# descritor(REGISTRADOR/buffer QUE GUARDA O ARQUIVO) em $v0
 	
+    		move $t0, $v0				# Copia do descritor
+	
+		li $v0, 15 				# ler o conteudo referenciado por $a0
+    		move $a0, $t0 				# descritor em $a0
+    		la $a1, output 				# conteudo a ser escrito
+    		li $a2, 512 				# tamanho do buffer = deve ser maior que o tam do arq
+    		syscall 				# leitura de TODO o arquivo, colocando a informação no buffer
 
-strlen:
-	li 	$t0, 0 				# int t0 = 0
-	li 	$t2, 0 				# char t2 = 0
-	li	$t4, 0
-	li	$t5, 11			
+    		li $v0, 16
+    		move $a0, $t0 				# Devolve pra a0 o descritor, necessário pra fechar o arquivo
+    		syscall
 	
-	strlen_loop:
-		add	$t2, $a0, $t0		# t2 = inicio_palavra + t0
-		lb	$t1, 0($t2)		# t1 = palavra[t0]
+		li 	$v0, 16
+		move 	$a0, $s0 			# Devolve pra a0 o descritor, necessário pra fechar o arquivo
+		syscall					# Fechar o arquivo
 		
-		slt	$t4, $t1, $t5		# t4 = 0 ou 1 (t1 < 65)
-		beq	$t4, 1, strlen_exit	
-		#beqz	$t1, strlen_exit	# if (t1 == null) {sair}
+		li	$v0, 10				# exit()
+		syscall
+	
+	find_word:
+		li	$t0, 0
+		li	$t2, 0
+		li	$t4, 0
 		
-		addiu	$t0, $t0, 1 		# t0++
-		j	strlen_loop		# volta pro loop
+		find_loop:
+			add	$t2, $a0, $t0		# t2 = inicio_palavra + t0
+			lb	$t1, 0($t2)		# t1 = palavra[t0]
+			slti	$t4, $t1, 11		# t4 = 0 ou 1 (t1 <= 10)
+			beq	$t4, 1, found		# if (t1<10){strlen_exit}
+			addiu	$t0, $t0, 1 		# s1++
+			j	find_loop		# volta pro loop
+			
+		found:
+			#subi 	$t0, $t0, 1
+			add	$v0, $zero, $t0
+			add	$t0, $zero, $zero
+			jr	$ra
+	
+	reverse_word:
+		li	$t0, 0				# t0 = primeiro caracter
+		li	$t3, 0				# t3 vai ser o caracter
+		li	$t5, 0				# Resultado IF
 		
-	strlen_exit:
-		subi	$t0, $t0, 1		# t0 -= 1
-		add	$v0, $zero, $t0		# v0 = t0 (tamanho da palavra)
-		add	$t0, $zero, $zero	# limpa t0
-		jr	$ra			# return;
+		li	$t4, 10
+		sb	$t4, output($t1)
+		subi	$t1, $t1, 1
+		
+		reverse_loop:
+			add	$t3, $t2, $t0		# t3 = palavra(t2) + t0
+			lb	$t4, 0($t3)		# t4 = palavra[t0]
+			
+			slti	$t5, $t4, 11		# t5 = 0 ou 1 (t1 <= 10)
+			beq	$t5, 1, reversed	# if (t1<10){strlen_exit}
+			
+			sb	$t4, output($t1)	# output[t1] = t4	
+			subi	$t1, $t1, 1		# t1--
+			addi	$t0, $t0, 1		# t0++
+			j	reverse_loop		# Loop until we reach our condition
+	
+		reversed:
+			jr	$ra
